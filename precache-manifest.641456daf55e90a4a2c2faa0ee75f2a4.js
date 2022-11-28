ea104d7d65e56c67a44c2ad6132a28fb5e930572
Etchu Ferdinand <ferdinandetchu@gmail.com>
2022-11-28 18:12:56 +0100

    Updates

---
+++ precache-manifest.641456daf55e90a4a2c2faa0ee75f2a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e254924d80111a7d219",
+    "url": "/static/css/main.d4e2da1e.chunk.css"
+  },
+  {
+    "revision": "2e254924d80111a7d219",
+    "url": "/static/js/main.2e254924.chunk.js"
+  },
+  {
+    "revision": "b9e1ee3007049d95a326",
+    "url": "/static/js/1.b9e1ee30.chunk.js"
+  },
+  {
+    "revision": "fcda57cf104e18d9888e",
+    "url": "/static/css/2.20267a8a.chunk.css"
+  },
+  {
+    "revision": "fcda57cf104e18d9888e",
+    "url": "/static/js/2.fcda57cf.chunk.js"
+  },
+  {
+    "revision": "fdc95eec5cb01cdc115a",
+    "url": "/static/js/runtime~main.fdc95eec.js"
+  },
+  {
+    "revision": "16f7863edadf84514541755b2eacb354",
+    "url": "/index.html"
+  }
+];
